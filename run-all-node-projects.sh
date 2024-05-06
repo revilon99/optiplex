db03415a943aa0f25f92e47febca0ec14f66e23f
Oli Cass <olicass99@gmail.com>
2024-05-06 11:55:41 +0100

    Update run-all-node-projects.sh

--- run-all-node-projects.sh
+++ run-all-node-projects.sh
@@ -1,9 +1,9 @@
 cd /home/ocass/optiplex/main/
 
 cd hello-world
-(nohup npm run run) > PID.txt &
+(nohup npm run run) > /dev/null 2>&1 & echo $! > PID.txt &
 cd ..
 
 cd hello-world-2
-(nohup npm run run) > PID.txt &
+(nohup npm run run) > /dev/null 2>&1 & echo $! > PID.txt &
 cd ..
