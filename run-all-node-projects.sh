c84f85fee04ea1bf7f1eb5e0df85032cf40a48db
Oli Cass <olicass99@gmail.com>
2024-05-23 12:20:52 +0100

    Update run-all-node-projects.sh

--- run-all-node-projects.sh
+++ run-all-node-projects.sh
@@ -3,9 +3,9 @@ cd /home/ocass/optiplex/
 cd $1
 
 cd hello-world
-nohup (npm run $1& 2>&1| tee npm.txt)
+nohup npm run $1& 2>&1| tee npm.txt
 cd ..
 
 cd auth
-nohup (npm run $1& 2>&1| tee npm.txt)
+nohup npm run $1& 2>&1| tee npm.txt
 cd ..
